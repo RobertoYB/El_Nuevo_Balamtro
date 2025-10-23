59660336772ff6905fd14fb6867081e3b9416212
RobertoYB <ryupit26@gmail.com>
2025-10-23 09:12:45 -0600

    Clase Angel Efren
    Se creo la clase Angel Efren Rosado Santaigo para recordar que es muy guapo.

---
+++ El_Nuevo_Balamtro/Classes/AngelEfrenRosadoSantiago.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace El_Nuevo_Balamtro.Classes;
+
+public class AngelEfrenRosadoSantiago
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Angel Efren es muy guapo.");
+  }
+}
