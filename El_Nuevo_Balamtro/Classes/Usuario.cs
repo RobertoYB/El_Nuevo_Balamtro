a1224c8a53b98de82adbeb9f96946a6cd1d785cf
RobertoYB <ryupit26@gmail.com>
2025-10-29 07:34:00 -0600

    Hay un angel en el cielo
    Y es Angel Efren Rosado Santiago

--- El_Nuevo_Balamtro/Classes/Usuario.cs
+++ El_Nuevo_Balamtro/Classes/Usuario.cs
@@ -4,5 +4,8 @@ namespace El_Nuevo_Balamtro.Classes;
 
 public class Usuario
 {
-
+  public void AngelEfrenHayUnAngel()
+  {
+    Console.WriteLine($"Hay un Angel en el cielo el que me protege.");
+  }
 }
