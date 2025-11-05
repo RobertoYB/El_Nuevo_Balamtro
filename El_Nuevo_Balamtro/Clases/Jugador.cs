1b1a6ee7142ff21bafa504fe60ee14c0d106fd44
AngelRosadoWTF <angsoft05@gmail.com>
2025-11-05 16:17:05 -0600

    clases/Jugador
    Definición de la clase "Jugador" en el apartado "Clases".

--- El_Nuevo_Balamtro/Clases/Jugador.cs
+++ El_Nuevo_Balamtro/Clases/Jugador.cs
@@ -1,8 +1,16 @@
 using System;
 
-namespace El_Nuevo_Balamtro.Clases;
-
-public class Jugador
+namespace El_Nuevo_Balamtro.Clases
 {
-
+    public abstract class Jugador<T> where T : Carta
+    {
+        public string Nombre { get; set; }
+        public List<T> Mano { get; set; } = new List<T>();
+        public Jugador(string nombre)
+        {
+            Nombre = nombre;
+        }
+        public abstract T JugarTurno();
+        public override string ToString() => Nombre;
+    }
 }
