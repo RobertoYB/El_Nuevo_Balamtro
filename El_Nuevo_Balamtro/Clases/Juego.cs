f520e86452a3c4e3f2803d788257674611637fd8
AngelRosadoWTF <angsoft05@gmail.com>
2025-11-05 07:29:28 -0600

    clases/Juego
    Definición de la clase "Juego" en el apartado de "/Clases".

--- El_Nuevo_Balamtro/Clases/Juego.cs
+++ El_Nuevo_Balamtro/Clases/Juego.cs
@@ -1,8 +1,18 @@
 using System;
 
-namespace El_Nuevo_Balamtro.Clases;
-
-public class Juego
+namespace El_Nuevo_Balamtro.Clases
 {
-
+    public abstract class Juego
+    {
+        public string Nombre { get; protected set; }
+        public Juego(string Nombre)
+        {
+            Nombre = nombre;
+        }
+        public abstract void Iniciar();
+        public virtual void MostrarTitulo()
+        {
+            Console.WriteLine($"\n===== {Nombre.ToUpper()} =====");
+        }
+    }
 }
