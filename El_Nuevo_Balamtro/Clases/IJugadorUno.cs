ed0412e0f26cba84aa9b0c98f3edf662290090f3
DumpyBot07 <15234903@modelo.edu.mx>
2025-11-09 16:37:03 -0600

    Interfaz Jugador para el juego uno
    interfaz para los jugadores de el juego uno

--- El_Nuevo_Balamtro/Clases/IJugadorUno.cs
+++ El_Nuevo_Balamtro/Clases/IJugadorUno.cs
@@ -2,8 +2,17 @@ using System;
 
 namespace El_Nuevo_Balamtro.Clases
 {
-  public interface IJugadorUno
-  {
-    
-  }
+    public interface IJugadorUno
+    {
+        string Nombre { get; set; }
+        List<CartaUno> Mano { get; }
+        int NumeroDeCartas { get; }
+        CartaUno DecidirJugada(CartaUno cartaEnMesa, IJugadorUno siguienteJugador);
+        void RecibirCarta(CartaUno carta);
+        void JugarCarta(CartaUno carta);
+        List<CartaUno> ObtenerCartasJugables(CartaUno cartaEnMesa);
+        bool HaGanado();
+        void GritarUno();
+        void ReiniciarMano();
+    }
 }
